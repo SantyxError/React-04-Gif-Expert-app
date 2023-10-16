1c4791e1c24a3eb9678e58af4501844c3e4036b9
Santiago <sanbemal@gmail.com>
2023-10-16 08:52:00 +0200

    78. Componente AddCategory

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 
 import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory'
 
 export const GifExpertApp = () => {
 
@@ -14,6 +15,7 @@ export const GifExpertApp = () => {
       <h1>GifExpertApp</h1>
 
       {/* Input */}
+      <AddCategory/>
 
       {/* Listado de gif */}
       <button onClick={onAddCategory}>Agregar</button>
